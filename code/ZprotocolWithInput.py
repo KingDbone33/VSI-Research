cd75c533018b51dad4190cfe422e2eaba3ff3497
Devon Njonjo <njonjo@usc.edu>
2022-07-19 14:37:17 -0700

    added comparison to the polts

--- code/ZprotocolWithInput.py
+++ code/ZprotocolWithInput.py
@@ -5,6 +5,7 @@ import requests
 
 # Stuff for defining and tracking winning
 win = 0
+interactive_win = 0
 round_count = 0
 
 # Definition of x, y, and b
@@ -30,13 +31,26 @@ micropayment_per_lottery = macro * win_prob
 
 
 #appendfile
-FileF = open('NumberReport.txt', 'a')
+FileF = open('NumberReport.txt', 'w')
 FileF.write(str(macro) + "\n")
 FileF.write(str(win_prob) + "\n")
 FileF.write(str(x) + "\n")
 FileF.write(str(y) + "\n")
 
-
+#Hashed 30000 times
+x_hashed_array = []
+y_hashed_array = []
+hashed_counter = 0
+x_hash = x
+y_hash = y 
+while hashed_counter < 30000:
+#makes hash
+  x_hash = str(hashlib.sha256(x_hash.encode()).hexdigest()) 
+  y_hash = str(hashlib.sha256(y_hash.encode()).hexdigest())    
+  #puts hash into array
+  x_hashed_array.append(x_hash)
+  y_hashed_array.append(y_hash)
+  hashed_counter += 1
 
 #The loop
 randomness_round = 0
@@ -67,11 +81,18 @@ while randomness_round < 100:
         else:
            print('You did not win this round.')
 
+        #Interactive win check
+        interactive_combined = int(x_hashed_array[current_round], 16) ^ int(y_hashed_array[current_round], 16)
+        if win_decimal == interactive_combined % win_prob_denom:
+            interactive_win += 1
+
         print('Current Lottery Round  is ' + str(current_round) + '.')
         print('Current total micropayment is worth $' + str(current_total_micropayment) + ' dollars.')
         print('Current total macropayment is worth $' + str(current_total_macropayment) + '.0 dollars.')
+        print('Current total interactive macropayment is worth $' + str(interactive_win*macro) + '.0 dollars.')
         print(' ')
-        FileF.write(str(current_round) + ' ' + str(current_total_micropayment) + ' ' + str(current_total_macropayment) + "\n")
+        FileF.write(str(current_round) + ' ' + str(current_total_micropayment) + ' ' + str(current_total_macropayment) + ' ' +
+                    str(interactive_win*macro) + "\n")
 
         lottery_round = lottery_round + 1        
         #Hashes the previousely hashed value
--- code/plotting_section.py
+++ code/plotting_section.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 x_rounds = []
 y_macro = []
 y_micro = []
+y_macro_int = []
 
 plotting = open('NumberReport.txt', 'r')
 macro = plotting.readline()
@@ -15,12 +16,14 @@ for line_number in range(30000):
     x_rounds.append( int( line[ 0 ] ) + 1 )
     y_micro.append( float( line[ 1 ] ) )
     y_macro.append( float( line[ 2 ] ) )
+    y_macro_int.append( float( line[ 3 ] ) )
 
 plt.title("Micropayment vs Macropayment")
 plt.xlabel('Round')
 plt.ylabel('Cumulative Payment Value')
 plt.plot(x_rounds, y_micro, label="Micropayment")
-plt.plot(x_rounds, y_macro, label="Macropayment")
+plt.plot(x_rounds, y_macro, label="ZProtocol Macropayment")
+plt.plot(x_rounds, y_macro_int, label="Interactive Macropayment")
 plt.legend( loc="upper right" )
   
 plt.show()
